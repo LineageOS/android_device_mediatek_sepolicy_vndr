0d01681af7b446da848917f710dacea08d6b94f3
Mashopy <eliasgheeraert@gmail.com>
2026-01-29 20:38:10 +0100

    sepolicy_vndr: Merge basic, bsp and modem sepolicy into base
    Change-Id: I0e26cc13c1862787e8260550c83365e05f4400bc

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -3,18 +3,9 @@ MTK_SEPOLICY_PATH := device/mediatek/sepolicy_vndr
 
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/vendor \
-    $(MTK_SEPOLICY_PATH)/bsp/vendor \
-    $(MTK_SEPOLICY_PATH)/modem
-
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/private \
-    $(MTK_SEPOLICY_PATH)/bsp/private \
-
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/public \
-    $(MTK_SEPOLICY_PATH)/bsp/public \
+BOARD_VENDOR_SEPOLICY_DIRS += $(MTK_SEPOLICY_PATH)/base/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MTK_SEPOLICY_PATH)/base/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(MTK_SEPOLICY_PATH)/base/public
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 BOARD_VENDOR_SEPOLICY_DIRS += $(MTK_SEPOLICY_PATH)/debug/vendor
