421ddba68267eee9be9750679e75f73acc555e1c
Mashopy <eliasgheeraert@gmail.com>
2026-01-29 15:17:12 +0100

    sepolicy_vndr: Prepare reorganizing SEPolicy structure
    This follows the new structure that MediaTek uses on their BSP starting from Android 13.
    
    basic, bsp and modem directories will be merged as base directory and debug directories
    will be moved out of it and merged as one, essentially following MediaTek's new structure.
    
    It also makes maintaining SEPolicy easier as there will be less directories to manage.
    
    Change-Id: I58d705e5098474054e880f436ef7f53a1520f986

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -4,28 +4,28 @@ MTK_SEPOLICY_PATH := device/mediatek/sepolicy_vndr
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/non_plat \
-    $(MTK_SEPOLICY_PATH)/bsp/non_plat \
+    $(MTK_SEPOLICY_PATH)/basic/vendor \
+    $(MTK_SEPOLICY_PATH)/bsp/vendor \
     $(MTK_SEPOLICY_PATH)/modem
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/plat_private \
-    $(MTK_SEPOLICY_PATH)/bsp/plat_private \
+    $(MTK_SEPOLICY_PATH)/basic/private \
+    $(MTK_SEPOLICY_PATH)/bsp/private \
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/plat_public \
-    $(MTK_SEPOLICY_PATH)/bsp/plat_public \
+    $(MTK_SEPOLICY_PATH)/basic/public \
+    $(MTK_SEPOLICY_PATH)/bsp/public \
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/bsp/debug/non_plat \
-    $(MTK_SEPOLICY_PATH)/basic/debug/non_plat
+    $(MTK_SEPOLICY_PATH)/bsp/debug/vendor \
+    $(MTK_SEPOLICY_PATH)/basic/debug/vendor
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/debug/plat_private \
-    $(MTK_SEPOLICY_PATH)/bsp/debug/plat_private
+    $(MTK_SEPOLICY_PATH)/basic/debug/private \
+    $(MTK_SEPOLICY_PATH)/bsp/debug/private
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(MTK_SEPOLICY_PATH)/basic/debug/plat_public \
-    $(MTK_SEPOLICY_PATH)/bsp/debug/plat_public
+    $(MTK_SEPOLICY_PATH)/basic/debug/public \
+    $(MTK_SEPOLICY_PATH)/bsp/debug/public
 endif
